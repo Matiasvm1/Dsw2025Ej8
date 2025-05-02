5c3457212df61ebbec66b5b5683d930aebf46bd2
Emilio Luna Jandar <lunaemilio269@gmail.com>
2025-05-02 17:18:51 -0300

    Creacion metodo Depositar en clase CajaDeAhorro

--- Domain/CajaDeAhorro.cs
+++ Domain/CajaDeAhorro.cs
@@ -14,5 +14,35 @@ public class CajaDeAhorro : CuentaBancaria
         base(numero, saldo, TipoCuenta.CajaDeAhorro, titulares)
     {
     }
+
+    public override void Depositar(decimal monto)
+    {
+        try
+        {
+            if (monto <= 0)
+            {
+                throw new MontoNoValido();
+            }
+            else if (Estado != Estado.Activa)
+            {
+                throw new CuentaNoActiva($"No se puede operar con la cuenta {Estado}");
+            }
+            else
+            {
+                Saldo += monto;
+                Console.WriteLine($"Deposito de ${monto} realizado con exito.");
+            }
+
+
+        }
+        catch (MontoNoValido ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (CuentaNoActiva ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
 }
 
