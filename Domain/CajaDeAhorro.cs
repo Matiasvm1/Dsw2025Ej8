463c791a6da3e78b12740a04ade346b0ff2bae2a
Emilio Luna Jandar <lunaemilio269@gmail.com>
2025-05-02 17:20:20 -0300

    Creacion metodo Retirar en clase CajaDeAhorro

--- Domain/CajaDeAhorro.cs
+++ Domain/CajaDeAhorro.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dsw2025Ej8.Errores;
 
 namespace Dsw2025Ej8.Domain;
 
@@ -41,6 +42,43 @@ public class CajaDeAhorro : CuentaBancaria
         }
         catch (CuentaNoActiva ex)
         {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    public override void Retirar(decimal monto)
+    {
+        try
+        {
+            if (monto <= 0)
+            {
+                throw new MontoNoValido();
+            }
+            else if (Estado != Estado.Activa)
+            {
+                throw new CuentaNoActiva($"No se puede operar con la cuenta {Estado}");
+            }
+            else if ((Saldo - monto) < 0)
+            {
+                throw new SaldoInsuficiente("La cuenta no cuenta con saldo para la operacion Solicitada. Fue Suspendida.");
+            }
+            else
+            {
+                Saldo -= monto;
+                Console.WriteLine($"Retiro de $ {monto} realizado con exito.");
+            }
+        }
+        catch (MontoNoValido ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (CuentaNoActiva ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (SaldoInsuficiente ex)
+        {
+            Estado = Estado.Suspendida;
             Console.WriteLine(ex.Message);
         }
     }
