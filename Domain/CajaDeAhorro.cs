94de1b6a6324dc5ec8ee5d84fdaef3056a96f89f
Emilio Luna Jandar <lunaemilio269@gmail.com>
2025-05-02 17:16:40 -0300

    Creacion constructor de clase CajaDeAhorro

--- Domain/CajaDeAhorro.cs
+++ Domain/CajaDeAhorro.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Dsw2025Ej8.Domain
+namespace Dsw2025Ej8.Domain;
+
+public class CajaDeAhorro : CuentaBancaria 
 {
-    internal class CajaDeAhorro
+    public decimal TasaDeIntereses { get; set; }
+
+    public CajaDeAhorro(string numero, decimal saldo, string[] titulares) :
+        base(numero, saldo, TipoCuenta.CajaDeAhorro, titulares)
     {
     }
 }
+
