9349f92807571e18a8fe6a4e3009f72215b0ce6f
Facundo Lopez <csrlitoslopez@gmail.com>
2025-05-02 17:59:17 -0300

    Creando clases de errores

---
+++ Errores/CuentaNoActiva.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Ej8.Errores
+{
+    internal class CuentaNoActiva : Exception
+    {
+        public CuentaNoActiva(string message) : base(message)
+        {
+        }
+    }
+}
